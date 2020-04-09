be3c7a48b773f3ed45054d61931333f46ca6bb5b
William GARCIA <garcia.rodriguez.william@gmail.com>
2020-04-09 13:37:47 +1000

    JS/WG - Renamed dockercoin to twkoin

--- 01-docker/01-01-docker/solution/generated.sh
+++ 01-docker/01-01-docker/solution/generated.sh
@@ -1,26 +1,26 @@
 set -eu
 
 docker search node
-docker build -t dockercoins_webui .
-docker images | grep dockercoins_webui
-docker inspect dockercoins_webui
-docker inspect -f '{{ .Config.ExposedPorts }}' dockercoins_webui
-docker history dockercoins_webui:latest
+docker build -t twkoins_webui .
+docker images | grep twkoins_webui
+docker inspect twkoins_webui
+docker inspect -f '{{ .Config.ExposedPorts }}' twkoins_webui
+docker history twkoins_webui:latest
 export TEAM_NAME=sandbox
-docker tag dockercoins_webui k8straining.azurecr.io/dockercoins_webui:${TEAM_NAME}
-docker images | grep dockercoins_webui
+docker tag twkoins_webui k8straining.azurecr.io/twkoins_webui:${TEAM_NAME}
+docker images | grep twkoins_webui
 az login
 az account set --subscription <subscription-id>
 az acr login --name k8straining
-docker push k8straining.azurecr.io/dockercoins_webui:${TEAM_NAME}
-docker rmi -f dockercoins_webui
-docker rmi -f k8straining.azurecr.io/dockercoins_webui:${TEAM_NAME}
+docker push k8straining.azurecr.io/twkoins_webui:${TEAM_NAME}
+docker rmi -f twkoins_webui
+docker rmi -f k8straining.azurecr.io/twkoins_webui:${TEAM_NAME}
 docker ps
-docker run -d -p 80:80 --name dockercoins_webui k8straining.azurecr.io/dockercoins_webui:${TEAM_NAME}
+docker run -d -p 80:80 --name twkoins_webui k8straining.azurecr.io/twkoins_webui:${TEAM_NAME}
 docker ps
-docker port dockercoins_webui
-docker exec dockercoins_webui ps -ef
-docker exec -ti dockercoins_webui bash -c "TERM=vt100 top -c"
-docker logs --follow --tail=20 dockercoins_webui
-docker kill dockercoins_webui
-docker rm dockercoins_webui
+docker port twkoins_webui
+docker exec twkoins_webui ps -ef
+docker exec -ti twkoins_webui bash -c "TERM=vt100 top -c"
+docker logs --follow --tail=20 twkoins_webui
+docker kill twkoins_webui
+docker rm twkoins_webui
