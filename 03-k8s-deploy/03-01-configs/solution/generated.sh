f7f74a06ded7d9a8120c0000cdc525898d620cb0
William GARCIA <garcia.rodriguez.william@gmail.com>
2020-05-06 21:06:18 +1000

    Removed all generated.sh scripts

--- 03-k8s-deploy/03-01-configs/solution/generated.sh
+++
@@ -1,5 +0,0 @@
-set -eux 
-
-kubectl apply -f config-map.yaml
-kubectl apply -f config-pod.yaml
-kubectl logs pod/app
--- 03-k8s-deploy/03-02-secrets/solution/generated.sh
+++
@@ -1,7 +0,0 @@
-set -eux 
-
-kubectl create secret generic my-secret --from-literal=password=helloWorld42+
-kubectl apply -f secret-pod.yaml
-kubectl logs pod/secret-app
-kubectl delete all --all -n "${TEAM_NAME}"
-kubectl delete secret my-secret
