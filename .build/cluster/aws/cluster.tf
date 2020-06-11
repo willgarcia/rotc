1a17b20f4b7fc50eaaba1c4740e9df032a0f9d31
Sarah Murphy <sarahnmurphy93@gmail.com>
2020-06-11 17:14:13 +1000

    aws cluster add iam policy resources

--- .build/cluster/aws/cluster.tf
+++ .build/cluster/aws/cluster.tf
@@ -28,3 +28,32 @@ resource "aws_subnet" "main_subnet_2" {
     Name = "main_subnet_2"
   }
 }
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster_role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
