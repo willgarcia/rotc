aea23b54108138e5067691b01dcbbaf3810be230
Sarah Murphy <sarahnmurphy93@gmail.com>
2020-06-12 14:34:23 +1000

    create aws cluster"

--- .build/cluster/aws/cluster.tf
+++ .build/cluster/aws/cluster.tf
@@ -14,6 +14,7 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "main_subnet_1" {
   vpc_id     = "${aws_vpc.main_vpc.id}"
   cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "main_subnet_1"
@@ -23,6 +24,7 @@ resource "aws_subnet" "main_subnet_1" {
 resource "aws_subnet" "main_subnet_2" {
   vpc_id     = "${aws_vpc.main_vpc.id}"
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "main_subnet_2"
@@ -56,4 +58,66 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
 resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_eks_cluster" "aws_eks" {
+  name     = "eks_cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = ["${aws_subnet.main_subnet_1.id}", "${aws_subnet.main_subnet_2.id}"]
+  }
+
+  tags = {
+    Name = "eks_cluster"
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    "aws_iam_role_policy_attachment.AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.AmazonEKSServicePolicy",
+  ]
+}
+
+output "endpoint" {
+  value = "${aws_eks_cluster.aws_eks.endpoint}"
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = "${aws_eks_cluster.aws_eks.certificate_authority.0.data}"
+}
+
+resource "aws_iam_role" "eks_nodes_role" {
+  name = "eks-node-group_role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_nodes_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_nodes_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodes_role.name
 }
