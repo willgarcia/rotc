ffbbf1cdbe15110a6079030d3ff4eee53dae4196
Malithi Jalathge <malithi.jalathge@thoughtworks.com>
2020-06-17 18:46:52 +1000

    Create node_group and Added Networking config

--- .build/cluster/aws/cluster.tf
+++ .build/cluster/aws/cluster.tf
@@ -20,6 +20,8 @@ resource "aws_s3_bucket" "servicemesh_bucket" {
 
 resource "aws_vpc" "servicemesh_vpc" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = "true"
+  enable_dns_support   = "true"
 
   tags = {
     Name = "servicemesh_vpc"
@@ -30,9 +32,10 @@ resource "aws_subnet" "servicemesh_subnet_1" {
   vpc_id     = "${aws_vpc.servicemesh_vpc.id}"
   cidr_block = "10.0.0.0/24"
   availability_zone = "${var.availability_zone_1}"
+  map_public_ip_on_launch = "true"
 
   tags = {
-    Name = "servicemesh_subnet_1"
+    Name = "servicemesh_subnet_1_public"
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
 }
@@ -41,11 +44,94 @@ resource "aws_subnet" "servicemesh_subnet_2" {
   vpc_id     = "${aws_vpc.servicemesh_vpc.id}"
   cidr_block = "10.0.1.0/24"
   availability_zone = "${var.availability_zone_2}"
+  map_public_ip_on_launch = "true"
 
   tags = {
-    Name = "servicemesh_subnet_2"
+    Name = "servicemesh_subnet_2_public"
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
+}
+
+resource "aws_internet_gateway" "servicemesh_igw" {
+    vpc_id = "${aws_vpc.servicemesh_vpc.id}"
+    tags = {
+        Name = "servicemesh_igw"
+    }
+}
+
+resource "aws_route_table" "servicemesh_public_crt" {
+    vpc_id = "${aws_vpc.servicemesh_vpc.id}"
+    
+    route {
+        //associated subnet can reach everywhere
+        cidr_block = "0.0.0.0/0" 
+        //CRT uses this IGW to reach internet
+        gateway_id = "${aws_internet_gateway.servicemesh_igw.id}" 
+    }
+    
+    tags = {
+        Name = "servicemesh_public_crt"
+    }
+}
+
+resource "aws_route_table_association" "servicemesh_crta_public_subnet_1"{
+    subnet_id = "${aws_subnet.servicemesh_subnet_1.id}"
+    route_table_id = "${aws_route_table.servicemesh_public_crt.id}"
+}
+
+resource "aws_route_table_association" "servicemesh_crta_public_subnet_2"{
+    subnet_id = "${aws_subnet.servicemesh_subnet_2.id}"
+    route_table_id = "${aws_route_table.servicemesh_public_crt.id}"
+}
+
+resource "aws_security_group" "servicemesh_sg" {
+    name = "servicemesh_sg"
+    vpc_id = "${aws_vpc.servicemesh_vpc.id}"
+    description = "Allow incoming HTTP connections."
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        // This means, all ip address are allowed to ssh ! 
+        // Do not do it in the production.
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "servicemesh_sg"
+    }
 }
 
 resource "aws_eks_cluster" "servicemesh_eks_cluster" {
@@ -54,6 +140,8 @@ resource "aws_eks_cluster" "servicemesh_eks_cluster" {
 
   vpc_config {
     subnet_ids = ["${aws_subnet.servicemesh_subnet_1.id}", "${aws_subnet.servicemesh_subnet_2.id}"]
+    endpoint_private_access = "true"
+    security_group_ids = ["${aws_security_group.servicemesh_sg.id}"]
   }
 
   tags = {
@@ -66,12 +154,4 @@ resource "aws_eks_cluster" "servicemesh_eks_cluster" {
     "aws_iam_role_policy_attachment.AmazonEKSClusterPolicy",
     "aws_iam_role_policy_attachment.AmazonEKSServicePolicy",
   ]
-}
-
-output "endpoint" {
-  value = "${aws_eks_cluster.servicemesh_eks_cluster.endpoint}"
-}
-
-output "kubeconfig-certificate-authority-data" {
-  value = "${aws_eks_cluster.servicemesh_eks_cluster.certificate_authority.0.data}"
 }
