d47aea913b92e1d379cc54431290c59fa957658a
Bruno Mazzo <bruno.mazzo@thoughtworks.com>
2020-06-23 15:31:37 +1000

    Change docker image for AWS setup

--- .build/scripts/aws/install-istio.sh
+++ .build/scripts/aws/install-istio.sh
@@ -1,22 +1,20 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 curl -sL https://git.io/getLatestIstio | sh -
-
 cd istio*
+
 export PATH=$PWD/bin:$PATH
+echo $PATH
 
 test -x /usr/local/bin/istioctl || cp bin/istioctl /usr/local/bin/
 
 echo "Istio version"
 istioctl version --remote=false
 
-# TO DO: Get below commands working within the bash script
-# Issue: aws/kubectl is not recognized as an executable within the script. 
-# Could be something to do with $HOME/#PATH within the script 
-
 # 1. Create a kubeconfig for Amazon EKS and verify configuration
-# aws eks --region us-east-1 update-kubeconfig --name servicemesh_eks_cluster
-# kubectl get svc
+aws eks --region us-east-1 update-kubeconfig --name servicemesh_eks_cluster
+
+kubectl get svc
 
 # 2. Install Istio in the istio-system namespace
 # echo "Istio pre-installation verification"
