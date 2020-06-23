62181ee68cc9dc44b3b12bb4005a34e2e6f782a8
Bruno Mazzo <bruno.mazzo@thoughtworks.com>
2020-06-23 16:06:34 +1000

    Add istio install commands

--- .build/scripts/aws/install-istio.sh
+++ .build/scripts/aws/install-istio.sh
@@ -14,15 +14,13 @@ istioctl version --remote=false
 # 1. Create a kubeconfig for Amazon EKS and verify configuration
 aws eks --region us-east-1 update-kubeconfig --name servicemesh_eks_cluster
 
-kubectl get svc
-
 # 2. Install Istio in the istio-system namespace
-# echo "Istio pre-installation verification"
-# istioctl verify-install
-# istioctl manifest apply --set profile=demo
+echo "Istio pre-installation verification"
+istioctl verify-install
+istioctl manifest apply --set profile=demo
 
 # 3. Verify the services are deployed
-# kubectl -n istio-system get svc
+kubectl -n istio-system get svc
 
 # 4. Verify Pods
-# kubectl -n istio-system get pods
+kubectl -n istio-system get pods
