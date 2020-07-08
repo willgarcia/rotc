d8a790de3f942a420dec40bdcad24fab228b7b7d
Sarah Murphy <sarahnmurphy93@gmail.com>
2020-07-08 16:05:44 +1000

    update canary file

--- .build/cluster/aws/EKS_Cluster/node_group.tf
+++ .build/cluster/aws/EKS_Cluster/node_group.tf
@@ -5,8 +5,8 @@ resource "aws_eks_node_group" "servicemesh_eks_node_group" {
   subnet_ids      = var.subnet_ids
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
   }
 }
