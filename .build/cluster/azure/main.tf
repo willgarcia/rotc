abd083048b7c7d7a4ad033530488882575ffe4bf
Nick Drew <nick.drew@thoughtworks.com>
2020-07-09 12:23:45 +1000

    Azure TF recipe - uses multi-stage docker for fun.
    Co-authored-by: donatoivan <donato.ivan@gmail.com>

---
+++ .build/cluster/azure/main.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you are using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+}
+
+terraform {
+    backend "azurerm" {
+        resource_group_name = "terraform-service-mesh"
+        storage_account_name = "stgterra1"
+        container_name = "tfstate"
+        key = "dev"
+    }
+}
