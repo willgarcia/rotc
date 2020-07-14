45495f5683425d61e84ae4f3daa4876651a4849b
Nick Drew <nick.drew@thoughtworks.com>
2020-07-14 12:08:05 +1000

    Basic kube cluster installation.
    Co-authored-by: donatoivan <donato.ivan@gmail.com>

---
+++ .build/cluster/azure/aks_cluster/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  type = string
+  default = "East US"
+}
+
+variable "prefix" {
+    type = string
+    description = "Give YOUR cluster a unique prefix (3-45 alpha characters)"
+}
