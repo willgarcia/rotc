a94b266a63ed1e221aab6b84b36fb1f1d9370cd6
Nick Drew <nick.drew@thoughtworks.com>
2020-07-14 14:56:18 +1000

    Partial work: azure app gateway internal network
    Co-authored-by: donatoivan <donato.ivan@gmail.com>

--- .build/cluster/azure/aks_cluster/cluster.tf
+++ .build/cluster/azure/aks_cluster/cluster.tf
@@ -1,38 +1,44 @@
-resource "azurerm_kubernetes_cluster" "servicemesh_aks_cluster" {
-  name                = "${var.prefix}-k8s"
-  location            = azurerm_resource_group.servicemesh_aks_cluster.location
-  resource_group_name = azurerm_resource_group.servicemesh_aks_cluster.name
-  dns_prefix          = "${var.prefix}-k8s"
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name       = var.aks_name
+  location   = data.azurerm_resource_group.rg.location
+  dns_prefix = var.aks_dns_prefix
 
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_DS2_v2"
-  }
+  resource_group_name = data.azurerm_resource_group.rg.name
 
-  identity {
-    type = "SystemAssigned"
+  linux_profile {
+    admin_username = var.vm_user_name
+
+    ssh_key {
+      key_data = file(var.public_ssh_key_path)
+    }
   }
 
   addon_profile {
-    aci_connector_linux {
-      enabled = false
-    }
-
-    azure_policy {
-      enabled = false
-    }
-
     http_application_routing {
       enabled = false
     }
-
-    kube_dashboard {
-      enabled = true
-    }
-
-    oms_agent {
-      enabled = false
-    }
   }
+
+  default_node_pool {
+    name            = "agentpool"
+    node_count      = var.aks_agent_count
+    vm_size         = var.aks_agent_vm_size
+    os_disk_size_gb = var.aks_agent_os_disk_size
+    vnet_subnet_id  = data.azurerm_subnet.kubesubnet.id
+  }
+
+  service_principal {
+    client_id     = var.aks_service_principal_app_id
+    client_secret = var.aks_service_principal_client_secret
+  }
+
+  network_profile {
+    network_plugin     = "azure"
+    dns_service_ip     = var.aks_dns_service_ip
+    docker_bridge_cidr = var.aks_docker_bridge_cidr
+    service_cidr       = var.aks_service_cidr
+  }
+
+  depends_on = [azurerm_virtual_network.test, azurerm_application_gateway.network]
+  tags       = var.tags
 }
