f0e91171a2a1d231d8a2497cb0a1b7c4db02d4dc
Charlie Brooking <cbrookin@thoughtworks.com>
2020-09-09 09:36:45 +1000

    exclude third-party markdown from generated toc
    limit depth to 3 as a simple solution
    
    avoids e.g. markdown from node_modules coming through

--- .build/scripts/toc.sh
+++ .build/scripts/toc.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 
 curl https://raw.githubusercontent.com/ekalinin/github-markdown-toc/master/gh-md-toc -o gh-md-toc
 chmod a+x gh-md-toc
-./gh-md-toc `find . -iname "*.md" | grep 0 | sort` >> README.md
+
+./gh-md-toc `find . -maxdepth 3 -and -iname "*.md" | grep 0 | sort` >> README.md
